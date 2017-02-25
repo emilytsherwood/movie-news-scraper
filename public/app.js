03b317098a32727cac7ab4badf334cf972b900a8
Emily Turner-Sherwood <emilyturner@Emilys-MacBook-Pro.local>
2017-02-25 09:40:08 -0600

    buttons working

--- public/app.js
+++ public/app.js
@@ -1,1 +1,9 @@
 //Where all the button functionality will go
+
+$.getJSON("/articles", function(data) {
+    // For each one...with a for loop
+    for (var i = 0; i < data.length; i++) {
+        $(".articles").append("<p data-id='" + data[i]._id + "'>" + data[i].link + "</p>");
+    }
+});
+
